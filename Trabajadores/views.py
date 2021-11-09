de7dbca313caf3fa56b6b8f1e4710c9ade6c58f3
camilo <cavb.1205@hotmail.com>
2021-11-09 17:59:47 -0300

    se extendio el modelo user para trabajador con perfil

--- Trabajadores/views.py
+++ Trabajadores/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from Servicios.models import Servicios
+from Trabajadores.models import Perfil
 from .forms import TrabajadorForm, PerfilForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def lista_trabajadores(request):
-    lista_trabajadores = User.objects.all().exclude(is_staff=True)
+    lista_trabajadores = User.objects.all().exclude(is_staff=True).order_by('-is_active')
     total_trabajadores = lista_trabajadores.count()
     for x in lista_trabajadores:
         print(x.groups.all())
@@ -30,11 +33,18 @@ def detalle_trabajador(request, trabajador_id):
 def crear_trabajador(request):
     if request.method == 'POST':
         userform = TrabajadorForm(request.POST)
-        perfilform = TrabajadorForm(request.POST)
+        perfilform = PerfilForm(request.POST)
         if userform.is_valid() and perfilform.is_valid():
+            p = perfilform.save(commit=False)
             usuario = userform.save()
-            perfilform.save()
-        return redirect('detalle_trabajador', trabajador_id=usuario.id)
+            perfil = Perfil.objects.create(trabajador=usuario, identificacion = p.identificacion, biografia = p.biografia, telefono = p.telefono)
+            usuario.save()
+            perfil.save()
+            messages.success(request, 'Trabajador creado con éxito.') 
+            return redirect('detalle_trabajador', trabajador_id=usuario.id)
+        else:
+            messages.warning(request, 'No se pudo crear el registro, por favor corregir los datos.') 
+            return render(request,'trabajador_form.html', {'userform':userform,'perfilform':perfilform})
     else:
         userform = TrabajadorForm()
         perfilform = PerfilForm()
